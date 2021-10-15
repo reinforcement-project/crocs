9a127b6d1089b6e8f2ea3f3e183f7fca8618af2c
Robert Crocker <robert@vizsimply.com>
2021-10-14 23:29:02 -0300

    Created a working settings page.

--- client/pages/SettingsPage/SettingsReg.js
+++ client/pages/SettingsPage/SettingsReg.js
@@ -1,19 +1,19 @@
+/* eslint-disable react/jsx-key */
 import React, { useEffect, useState } from "react";
-import SkillAdmin from "../../components/Skill/SkillAdmin";
+import { Checkbox } from "./CheckboxSettings";
+import styled from "styled-components";
 
-const SettingsReg = () => {
-  //holds newSkills state
-  const [newSkills, setNewSkills] = useState([]);
-  //holds skills user already teaches
-  const [userSkills, setUserSkills] = useState([]);
-  //states below represent different errors
+const SettingsReg = (props) => {
+  // holds the new skills the user may want to teach
+  const [availableSkills, setAvailableSkills] = useState([]);
+  // holds skills the user already teaches
+  const [currentSkills, setCurrentSkills] = useState([]);
+  // states below represent different errors
   const [error, setError] = useState(false);
   const [errorExist, setErrorExist] = useState(false);
   const [errorEmail, setErrorEmail] = useState(false);
   const [wrongEmail, setWrongEmail] = useState(false);
-
-  const emailLS = localStorage.getItem("email");
-  const [email, setNewEmail] = useState(emailLS);
+  const [email, setNewEmail] = useState(localStorage.getItem("email"));
   const [emailChange, setEmailChange] = useState(false);
 
   // email validation func, returns boolean;
@@ -72,32 +72,34 @@ const SettingsReg = () => {
 
   // fetches data on mount;
   // GET request;
-  // sorting skills to 2 states - newSkills and userSkills by finding if user id is present in skills teachers array of objects;
+  // sorting skills to 2 states - availableSkills and currentSkills by finding if user id is present in skills teachers array of objects;
 
   const fetchData = async () => {
     try {
       const res = await fetch("/api/allSkills/all");
       const response = await res.json();
       response.sort((a, b) => (a.name > b.name ? 1 : -1));
-      const availableSkills = [];
-      const thisSkills = [];
+      const userAvailableSkills = [];
+      const userCurrentSkills = [];
       for (let i = 0; i < response.length; i++) {
         for (let j = 0; j < response[i].teachers.length; j++) {
           if (response[i].teachers[j].email === email) {
-            thisSkills.push(response[i]);
+            userCurrentSkills.push(response[i]);
             break;
           }
         }
-        if (thisSkills.length === 0) {
-          availableSkills.push(response[i]);
+        if (userCurrentSkills.length === 0) {
+          userAvailableSkills.push(response[i]);
         } else if (
-          thisSkills[thisSkills.length - 1].name !== response[i].name
+          userCurrentSkills[userCurrentSkills.length - 1].name !==
+          response[i].name
         ) {
-          availableSkills.push(response[i]);
+          userAvailableSkills.push(response[i]);
         }
       }
-      setNewSkills(availableSkills);
-      setUserSkills(thisSkills);
+      // update state: availableSkills and currentSkills
+      setAvailableSkills(userAvailableSkills);
+      setCurrentSkills(userCurrentSkills);
     } catch (err) {
       console.log(err);
     }
@@ -105,7 +107,7 @@ const SettingsReg = () => {
 
   const emailTyped = (e) => {
     if (e.target.value === "") {
-      setNewEmail(emailLS);
+      setNewEmail(localStorage.getItem("email"));
     } else {
       setNewEmail(e.target.value);
     }
@@ -113,7 +115,7 @@ const SettingsReg = () => {
 
   const updateEmail = async () => {
     try {
-      if (!validateEmail(email) || email === emailLS) {
+      if (!validateEmail(email) || email === localStorage.getItem("email")) {
         setWrongEmail(true);
         return;
       }
@@ -122,7 +124,10 @@ const SettingsReg = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ newEmail: email, currentEmail: emailLS }),
+        body: JSON.stringify({
+          newEmail: email,
+          currentEmail: localStorage.getItem("email"),
+        }),
       });
       const data = await res.json();
       console.log("data:", data);
@@ -141,150 +146,98 @@ const SettingsReg = () => {
     }
   };
 
-  //triggered in SkillAdmin Component;
-  //sends POST request to add the skill;
-  //response - new list of all skills;
-  //sorting skills to 2 states - newSkills and userSkills by finding if user id is present in skills teachers array of objects
-  const addNewSkill = async (arg) => {
-    try {
-      console.log({ skillName: arg, email: email });
-      const res = await fetch("api/adduserskill", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ skillName: arg, email: email }),
-      });
-      const response = await res.json();
-      response.sort((a, b) => (a.name > b.name ? 1 : -1));
-      const availableSkills = [];
-      const thisSkills = [];
-      for (let i = 0; i < response.length; i++) {
-        for (let j = 0; j < response[i].teachers.length; j++) {
-          if (response[i].teachers[j].email === email) {
-            thisSkills.push(response[i]);
-            break;
-          }
-        }
-        if (thisSkills.length === 0) {
-          availableSkills.push(response[i]);
-        } else if (
-          thisSkills[thisSkills.length - 1].name !== response[i].name
-        ) {
-          availableSkills.push(response[i]);
-        }
-      }
-      setNewSkills(availableSkills);
-      setUserSkills(thisSkills);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const Wrapper = styled.div`
+    margin: 40px auto 0px auto;
+    width: 80%;
+  `;
 
-  //triggered in SkillAdmin Component;
-  //sends delete request to delete the skill;
-  //response - new list of all skills;
-  //sorting skills to 2 states - newSkills and userSkills by finding if user id is present in skills teachers array of objects
-  const deleteUserSkill = async (arg) => {
-    try {
-      const res = await fetch("api/deleteuserskill", {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ skillName: arg, email: email }),
-      });
-      const response = await res.json();
-      response.sort((a, b) => (a.name > b.name ? 1 : -1));
-      const availableSkills = [];
-      const thisSkills = [];
-      for (let i = 0; i < response.length; i++) {
-        for (let j = 0; j < response[i].teachers.length; j++) {
-          if (response[i].teachers[j].email === email) {
-            thisSkills.push(response[i]);
-            break;
-          }
-        }
-        if (thisSkills.length === 0) {
-          availableSkills.push(response[i]);
-        } else if (
-          thisSkills[thisSkills.length - 1].name !== response[i].name
-        ) {
-          availableSkills.push(response[i]);
-        }
-      }
-      setNewSkills(availableSkills);
-      setUserSkills(thisSkills);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+    gap: 0px 20px;
+    grid-template-areas: ". .";
+  `;
+
+  const Gap = styled.hr`
+    margin-top: 40px;
+    margin-bottom: 40px;
+    border: 1px solid #d8ddfd;
+  `;
+
+  const PageHeading = styled.h1`
+    color: #171717;
+  `;
+
+  const SectionHeading = styled.h2`
+    color: #171717;
+  `;
+
+  const Paragraph = styled.p`
+    color: #171717;
+  `;
 
   return (
-    <div className="admin-settings-internal">
-      <div className="updateEmail">
-        <div className="form-title">UPDATE EMAIL</div>
-        {emailChange && (
-          <div className="email-changed">Email successfully updated</div>
-        )}
+    <Wrapper>
+      <div>
+        <PageHeading>Settings</PageHeading>
+        <Paragraph>
+          The adjustments made here will take effect as soon as they are set.
+        </Paragraph>
+      </div>
+      <Gap />
+      <div>
+        <SectionHeading>Email</SectionHeading>
+        {emailChange && <div>Email successfully updated</div>}
         {errorEmail && (
-          <div className="skill-add-error">
-            Technical error occured. Please contact Support
+          <div>Technical error occured. Please contact Support</div>
+        )}
+        {wrongEmail && <div>Please enter correct email</div>}
+        <Grid>
+          <Paragraph>Update your email here.</Paragraph>
+          <div>
+            <input type="text" placeholder={`${email}`} onChange={emailTyped} />
+            <button type="button" onClick={updateEmail}>
+              Update
+            </button>
           </div>
-        )}
-        {wrongEmail && (
-          <div className="skill-add-error">Please enter correct email</div>
-        )}
-        <form className="change-email-form">
-          <input
-            type="text"
-            className="update-email-input"
-            placeholder={`current email: ${email}`}
-            onChange={emailTyped}
-          />
-          <button type="button" className="update-email" onClick={updateEmail}>
-            Update
-          </button>
-        </form>
+        </Grid>
       </div>
-
-      <div className="user-settings-newskills">
-        <div className="form-title">ADD SKILLS TO TEACH</div>
-        {newSkills.length === 0 && (
-          <div className="noskills">No new skills at this time</div>
-        )}
-        <div className="listofskills">
-          {newSkills.map((skill) => {
+      <Gap />
+      <SectionHeading>Skills</SectionHeading>
+      <Grid>
+        <Paragraph>
+          Add or remove skills you would like to teach be checking the boxes.
+        </Paragraph>
+        <div>
+          {availableSkills.map((skill) => {
             return (
-              <SkillAdmin
+              <Checkbox
+                type="checkbox"
                 key={skill._id}
-                handleClick={addNewSkill}
-                functionality="+"
-                name={skill.name}
+                email={email}
+                setAvailableSkills={setAvailableSkills}
+                setCurrentSkills={setCurrentSkills}
+                label={skill.name}
+              />
+            );
+          })}
+          {currentSkills.map((skill) => {
+            return (
+              <Checkbox
+                type="checkbox"
+                key={skill._id}
+                email={email}
+                isChecked={true}
+                setAvailableSkills={setAvailableSkills}
+                setCurrentSkills={setCurrentSkills}
+                label={skill.name}
               />
             );
           })}
         </div>
-      </div>
-      <div className="user-settings-newskills">
-        <div className="form-title">SKILLS YOU TEACH</div>
-        {userSkills.length === 0 && (
-          <div className="noskills">No data to display at this time</div>
-        )}
-        <div className="listofskills">
-          {userSkills.map((skill) => {
-            return (
-              <SkillAdmin
-                key={skill._id}
-                functionality="-"
-                handleClick={deleteUserSkill}
-                name={skill.name}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </div>
+      </Grid>
+    </Wrapper>
   );
 };
 
