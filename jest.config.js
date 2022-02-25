adba3bf770df7098dc634b91634e0280a50bd778
Robert Crocker <robert@vizsimply.com>
2022-02-25 00:42:51 -0300

    Currently getting an error due to an inability to use modules with the current config.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-  transform: {},
+  transform: {
+    "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["node_modules/(?!variables/.*)"],
 };
