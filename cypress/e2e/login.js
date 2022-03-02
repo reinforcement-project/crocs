75108620cc720859963e26f724878f69b5a74988
Robert Crocker <robert@vizsimply.com>
2022-03-02 01:45:42 -0300

    Updated the script to run cypress in development mode or prod. Also updated the config to point to localhost:8080 to avoid the refresh when cypress loads.

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -1,12 +1,12 @@
-describe("Login form", () => {
-  it("can log me in", () => {
-    cy.visit("http://localhost:8080/")
+describe("Login Form", () => {
+  it("logs us into the application", () => {
+    cy.visit("/")
 
     cy.get(".ButtonGroup__GroupElement-sc-1vz6lai-0 > :nth-child(1)").click()
 
     cy.get('[for="email-input"] > [data-testid="input-component"]')
       .click()
-      .type("admin@test.com")
+      .type("user@test.com")
 
     cy.get('[for="password-input"] > [data-testid="input-component"]')
       .click()
